ea74e809b496cdbfc91c3c7b6446314e2726a33d
segnindenin <coulibalyoumar260@gmail.com>
2023-08-26 14:27:49 +0000

    permission pythonnaywhere

--- AllakroCommunity/settings.py
+++ AllakroCommunity/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9=$^ix)bvcslfbyl89w+aew#x^uquj3*$yq-_klkz%bw13^ep#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
